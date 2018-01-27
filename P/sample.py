77176e768e43a8cee997ff139e9bbea8bfc3cbbe
U-PATRICK\patrick_huy <pqhuy@bizzins.com>
2018-01-27 22:13:53 +0700

    ad fpgrowth

--- P/sample.py
+++ P/sample.py
@@ -5,11 +5,26 @@ import numpy as np
 import dataProcess.dataIO.read as ior
 import dataProcess.dataIO.write as iow
 import dataProcess.ADMM as admm
+import dataProcess.fpgrowth as fpg
 
-print("hello")
+if __name__ == "__main__":
+	print("main start")
 
-iow.write("test write")
-
-ior.read("test read")
-
-admm.run("ADMM")
+	iow.write("test write")
+	ior.read("test read")
+	admm.run("ADMM")
+	
+	transactions = [[1, 2, 5],
+                [2, 4],
+                [2, 3],
+                [1, 2, 4],
+                [1, 3],
+                [2, 3],
+                [1, 3],
+                [1, 2, 3, 5],
+                [1, 2, 3]]
+	print(" transactions: "+ str(transactions))
+	patterns = fpg.find_frequent_patterns(transactions, 2)
+	print(" patterns: "+ str(patterns))
+	rules = fpg.generate_association_rules(patterns, 0.7)
+	print(" rules: " + str(rules))
