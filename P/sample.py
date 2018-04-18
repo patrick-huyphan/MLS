e8c74badd662c08c215e708342ad8c3945b76c16
Your Name <huyphan.aj@gmail.com>
2018-04-18 22:15:02 +0700

    fpt

--- P/sample.py
+++ P/sample.py
@@ -13,25 +13,11 @@ import dataProcess.ADMM as admm
 import dataProcess.ASCC as ascc
 import dataProcess.fpgrowth as fpg
 import dataProcess.batchFP as bfg
+import dataProcess.linearRegresion as linear
 
 from numpy import arange,array,ones,linalg
 from pylab import plot,show
 from scipy import sparse
-
-def lnr():
-    xi = arange(0,9)
-    A = array([ xi, ones(9)])
-    # linearly generated sequence
-    y = [19, 20, 20.5, 21.5, 22, 23, 23, 25.5, 24]
-    w = linalg.lstsq(A.T,y)[0] # obtaining the parameters
-
-    # plotting the line
-    line = w[0]*xi+w[1] # regression line
-    plot(xi,line,'r-',xi,y,'o')
-    show()
-
-
-
 
 def runADMM():
     mat = ior.rawData2matrix(path+"data_694_446.dat",0, 446, 696)
@@ -56,7 +42,7 @@ if __name__ == "__main__":
     else:
         path = winDataPath()
     
-    #lnr()
+    #linear.lnr()
     '''
     transactions = [[1, 2, 5],
                 [2, 4],
